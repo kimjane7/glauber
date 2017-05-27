fd7634158b58e04b340c2f2d91bdca47e6fb51ae
Jane Kim <kimjane7@msu.edu>
2017-05-27 03:13:49 -0400

    Added CGlauber class.

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <cmath>
+#include "glauber.h"
+
+int main(int argc, char *argv[]){
+	
+	CGlauber *Au = new CGlauber(158);
+	CGlauber *U = new CGlauber(235);
+
+	printf("\n%s\n","-------- GOLD --------");
+	Au->random_test(5);
+
+	printf("\n%s\n","-------- URANIUM --------");
+	U->random_test(5);
+
+	return 0;
+}
