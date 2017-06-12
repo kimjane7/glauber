494397484cf59ecd7c69b13b7f30e28c03760f63
Jane Kim <kimjane7@msu.edu>
2017-06-12 08:59:40 -0400

    Printed energy densities to file.

--- glauber.h
+++ glauber.h
@@ -3,6 +3,7 @@
 #include <cmath>
 #include <cstdlib>
 #include <fstream>
+#include <string>
 
 
 const double pi = 4.0*atan(1.0);
@@ -11,8 +12,8 @@ using namespace std;
 class CNucleus{
 public:
 	
-	const static double a_ = 0.54;
-	const static double rho0_ = 0.17;
+	const static double a_ = 0.546;
+	const static double rho0_ = 0.159;
 	double R_;
 	int A_;
 
@@ -53,6 +54,7 @@ double CNucleus::get_R(){
 
 		// calculate A'(R)
 		A_prime = 4.0*pi*rho0_*R_*R_;
+		
 
 		// update radius estimate
 		R_ += (A_-A)/A_prime;
@@ -66,7 +68,7 @@ double CNucleus::get_R(){
 	for(r = 0.0; r < 2.5*R_; r+=dr){
 		A += 4.0*pi*get_rho(r)*r*r*dr;
 	}
-	printf("R = %lf, A from integrating = %lf\n", R_, A);
+	//printf("R = %lf, A from integrating = %lf\n", R_, A);
 
 	return R_;
 }
@@ -104,26 +106,22 @@ double CNucleus::get_T(double x, double y){
 	return T;
 }
 
-
-
-class CPairs {
+class CPairs{
 public:
 	
-	const static double sig_nn = 42.0; 
 	CNucleus *N1_, *N2_;
-	double dEdy_, sig_sat_, fwn_, b_;
+	double norm_, sig_sat_, fwn_, b_;
 
-	// declare member functions
-	CPairs(CNucleus *N1_set, CNucleus *N2_set, double b_set, 
-	double dEdy_set, double sig_sat_set, double fwn_set);
+	CPairs(CNucleus *N1_set, CNucleus *N2_set, double b_set, double sig_sat_set, double fwn_set);
+	double normalize();
 	double get_eps_wn(double x, double y);
 	double get_eps_sat(double x, double y);
 	double get_eps(double x, double y);
+	void print_eps(double min, double max, int nmax);
 
 };
 
-CPairs::CPairs(CNucleus *N1_set, CNucleus *N2_set, double b_set, 
-	double dEdy_set, double sig_sat_set, double fwn_set) {
+CPairs::CPairs(CNucleus *N1_set, CNucleus *N2_set, double b_set, double sig_sat_set, double fwn_set){
 
 	// set nucleii and impact parameter
 	N1_ = N1_set;
@@ -131,56 +129,96 @@ CPairs::CPairs(CNucleus *N1_set, CNucleus *N2_set, double b_set,
 	b_ = b_set;
 
 	// set parameters
-	dEdy_ = dEdy_set;
 	sig_sat_ = sig_sat_set;
-	fwn_ = fwn_set; 
+	fwn_ = fwn_set;
+
+	printf("%s\n","Normalizing...");
+	norm_ = normalize();
 }
 
-double CPairs::get_eps_wn(double x, double y) {
+double CPairs::normalize(){
+
+	double x, y, dx = 0.1, dy = dx;
+	double R1, R2, Rmax, eps, sum = 0.0;
+
+	R1 = N1_->get_R();
+	R2 = N2_->get_R();
+	if(R1 >= R2) Rmax = R1;
+	else Rmax = R2; 
+
+	for(y = -1.2*Rmax; y < 1.2*Rmax; y += dy){
+		for(x = -R2; x < R1; x += dx){
+			eps = fwn_*get_eps_wn(x,y)+(1.0-fwn_)*get_eps_sat(x,y);
+			sum += eps*dx*dy;
+			printf("sum = %lf\n",sum);
+		}		
+	}
+
+	norm_ = 1.0/sum;
+
+	printf("Normalization Constant = %lf\n", norm_);
+
+	return norm_;
+}
+
+double CPairs::get_eps_wn(double x, double y){
 
 	double prefactor, T1, T2, eps_wn;
 
 	// N1 centered at (-b/2,0) and N2 centered at (b/2,0)
-	prefactor = 0.5*dEdy_*sig_nn/sig_sat_;
 	T1 = N1_->get_T(x+0.5*b_,y);
 	T2 = N2_->get_T(x-0.5*b_,y);
 
 	// calculate wounded-nucleon energy density
-	eps_wn = prefactor*(T1*(1.0-exp(-T2*sig_sat_))+T2*(1.0-exp(-T1*sig_sat_)));
-	
+	eps_wn = (0.5/sig_sat_)*(T1*(1.0-exp(-T2*sig_sat_))+T2*(1.0-exp(-T1*sig_sat_)));
+
 	return eps_wn;
 }
 
-double CPairs::get_eps_sat(double x, double y) {
+double CPairs::get_eps_sat(double x, double y){
 
-	double prefactor, T1, T2, Tmin, Tmax, eps_sat;
+	double T1, T2, Tmin, Tmax, eps_sat;
 
 	// N1 centered at (-b/2,0) and N2 centered at (b/2,0)
-	prefactor = dEdy_*sig_nn/sig_sat_;
 	T1 = N1_->get_T(x+0.5*b_,y);
 	T2 = N2_->get_T(x-0.5*b_,y);
-	Tmin = 2.0*T1*T2/(T1+T2);
+
+	if(T1+T2==0.0) Tmin = 0.0;
+	else Tmin = 2.0*T1*T2/(T1+T2);
 	Tmax = 0.5*(T1+T2);
 
 	// calculate saturation energy density
-	eps_sat = prefactor*Tmin*(1.0-exp(-Tmax*sig_sat_));
+	eps_sat = (1.0/sig_sat_)*Tmin*(1.0-exp(-Tmax*sig_sat_));
+
+	//printf("T1 = %lf\tT2 = %lf\tTmin = %lf\tTmax = %lf\t eps_sat = %lf\n", T2,T2,Tmin,Tmax,eps_sat);
 	
 	return eps_sat;
 }
 
-double CPairs::get_eps(double x, double y) {
+double CPairs::get_eps(double x, double y){
 	
 	// calculate weighted average energy density
-	double eps = fwn_*get_eps_wn(x,y)+(1.0-fwn_)*get_eps_sat(x,y);
+	double eps = norm_*(fwn_*get_eps_wn(x,y)+(1.0-fwn_)*get_eps_sat(x,y));
 
 	return eps;
 }
 
-/*
+void CPairs::print_eps(double min, double max, int nmax){
 
-print a grid of x,y energy density
-same file format as liam's
+	double x, y, dx = (max-min)/nmax, dy = dx;
 
-*/
+	// open file
+	ofstream file;
+	file.open("energy_density.dat");
+
+	// print grid of energy densities
+	for(y = min; y < max; y += dy){
+		for(x = min; x < max; x += dx){
+			file << get_eps(x,y) << "\t";
+		}
+		file << "\n";
+	}
+}
+
 
 #endif
--- main.cpp
+++ main.cpp
@@ -2,31 +2,13 @@
 
 
 int main(int argc, char *argv[]){
-
-	// check for correct number of arguments
-	if (argc != 4){
-        printf("Input dE/dy, saturation cross section, and weight fwn.\n");
-        exit(-1);
-	}
-
-	// energy per rapidity, varies from 0.85-1.2
-	double dEdy = atof(argv[1]);
-
-	// saturation cross section, varies from 30-50 mb
-	double sigma_sat = 100.0*atof(argv[2]);
-
-	// wounded nucleon vs saturation model weight
-	double f_wn = atof(argv[3]); // Varies from 0-1
 	
 	// make new nuclei
-	CNucleus *Au = new CNucleus(197);
-	CNucleus *Pb = new CNucleus(207);
-	CNucleus *U = new CNucleus(235);
+	CNucleus *Pb1 = new CNucleus(207);
+	CNucleus *Pb2 = new CNucleus(207);
 
-	// print radii
-	//printf("Radius of Au-197 = %lf fm\n", Au->get_R());
-	//printf("Radius of Pb-207 = %lf fm\n", Pb->get_R());
-	//printf("Radius of U-235 = %lf fm\n", U->get_R());
+	// make Pb-Pb nuclei pair with 10.0 fm impact parameter
+	CPairs *Pb_Pb = new CPairs(Pb1, Pb2, 10.0, 40.0, 0.5);
 
 	return 0;
 }
