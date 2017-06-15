0f74ada134887a8373db33cbc1c3bcb4eea80838
MrHelloBye <mrhellobye@gmail.com>
2017-06-15 14:50:14 -0400

    Changed to c++11 because of additional useful features

--- glauber.h
+++ glauber.h
@@ -10,11 +10,11 @@ const double pi = 4.0*atan(1.0);
 
 namespace glauber {
 
-class CNucleus{
+class CNucleus {
 public:
 	
-	const static double a_ = 0.546;
-	const static double rho0_ = 0.159;
+	constexpr static double a_ = 0.546; // only constexpr, const enum, and const int values
+	constexpr static double rho0_ = 0.159; // are allowed to be set in class definition
 	double R_;
 	int A_;
 
@@ -26,7 +26,7 @@ public:
 
 };
 
-CNucleus::CNucleus(int A_set){
+CNucleus::CNucleus(int A_set) {
 	
 	// set the nucleon number
 	A_ = A_set;
@@ -35,7 +35,7 @@ CNucleus::CNucleus(int A_set){
 	R_ = get_R();
 }
 
-double CNucleus::get_R(){
+double CNucleus::get_R() {
 
 	double r, dr = 1.0E-3;
 	double A, A_prime;
@@ -45,11 +45,11 @@ double CNucleus::get_R(){
 	R_ = pow(3.0*A_/(4.0*pi*rho0_), 1.0/3.0);
 
 	// perform Newton's method to find R s.t. A(R)=A_
-	do{
+	do {
 
 		// calculate A(R)
 		A = 0.0;
-		for(r = 0.0; r < 2.5*R_; r += dr){
+		for(r = 0.0; r < 2.5*R_; r += dr) {
 			A += 4.0*pi*get_rho(r)*r*r*dr;
 		}
 
@@ -66,7 +66,7 @@ double CNucleus::get_R(){
 
 	// check if integrating gives expected nucleon number
 	A = 0.0;
-	for(r = 0.0; r < 2.5*R_; r+=dr){
+	for(r = 0.0; r < 2.5*R_; r+=dr) {
 		A += 4.0*pi*get_rho(r)*r*r*dr;
 	}
 	//printf("R = %lf, A from integrating = %lf\n", R_, A);
@@ -74,7 +74,7 @@ double CNucleus::get_R(){
 	return R_;
 }
 
-double CNucleus::get_rho(double r){
+double CNucleus::get_rho(double r) {
 
 	// calculate Woods-Saxon density
 	double rho = rho0_/(1.0+exp((r-R_)/a_));
@@ -82,7 +82,7 @@ double CNucleus::get_rho(double r){
 	return rho;
 }
 
-double CNucleus::get_rho(double x, double y, double z){
+double CNucleus::get_rho(double x, double y, double z) {
 	
 	// calculate distance from center of nucleus
 	double r = pow(x*x+y*y+z*z,0.5);
@@ -93,7 +93,7 @@ double CNucleus::get_rho(double x, double y, double z){
 	return rho;
 }
 
-double CNucleus::get_T(double x, double y){
+double CNucleus::get_T(double x, double y) {
 
 	// initialize variables
 	double z, z_bound = 2.0*pow(R_*R_-x*x-y*y,0.5), dz = 1.0E-3;
@@ -107,13 +107,14 @@ double CNucleus::get_T(double x, double y){
 	return T;
 }
 
-class CPairs{
+class CPairs {
 public:
 	
 	CNucleus *N1_, *N2_;
 	double norm_, sig_sat_, fwn_, b_;
 
-	CPairs(CNucleus *N1_set, CNucleus *N2_set, double b_set, double sig_sat_set, double fwn_set);
+	CPairs(CNucleus *N1_set, CNucleus *N2_set, double b_set,
+		double sig_sat_set, double fwn_set);
 	double normalize();
 	double get_eps_wn(double x, double y);
 	double get_eps_sat(double x, double y);
@@ -122,7 +123,8 @@ public:
 
 };
 
-CPairs::CPairs(CNucleus *N1_set, CNucleus *N2_set, double b_set, double sig_sat_set, double fwn_set){
+CPairs::CPairs(CNucleus *N1_set, CNucleus *N2_set, double b_set,
+		double sig_sat_set, double fwn_set) {
 
 	// set nucleii and impact parameter
 	N1_ = N1_set;
@@ -137,7 +139,7 @@ CPairs::CPairs(CNucleus *N1_set, CNucleus *N2_set, double b_set, double sig_sat_
 	norm_ = normalize();
 }
 
-double CPairs::normalize(){
+double CPairs::normalize() {
 
 	double x, y, dx = 0.1, dy = dx;
 	double R1, R2, Rmax, eps, sum = 0.0;
@@ -147,7 +149,7 @@ double CPairs::normalize(){
 	if(R1 >= R2) Rmax = R1;
 	else Rmax = R2; 
 
-	for(y = -1.2*Rmax; y < 1.2*Rmax; y += dy){
+	for(y = -1.2*Rmax; y < 1.2*Rmax; y += dy) {
 		for(x = -R2; x < R1; x += dx){
 			eps = fwn_*get_eps_wn(x,y)+(1.0-fwn_)*get_eps_sat(x,y);
 			sum += eps*dx*dy;
@@ -162,7 +164,7 @@ double CPairs::normalize(){
 	return norm_;
 }
 
-double CPairs::get_eps_wn(double x, double y){
+double CPairs::get_eps_wn(double x, double y) {
 
 	double prefactor, T1, T2, eps_wn;
 
@@ -171,12 +173,13 @@ double CPairs::get_eps_wn(double x, double y){
 	T2 = N2_->get_T(x-0.5*b_,y);
 
 	// calculate wounded-nucleon energy density
-	eps_wn = (0.5/sig_sat_)*(T1*(1.0-exp(-T2*sig_sat_))+T2*(1.0-exp(-T1*sig_sat_)));
+	eps_wn = (0.5/sig_sat_)*(T1*(1.0-exp(-T2*sig_sat_))
+		+ T2*(1.0-exp(-T1*sig_sat_)));
 
 	return eps_wn;
 }
 
-double CPairs::get_eps_sat(double x, double y){
+double CPairs::get_eps_sat(double x, double y) {
 
 	double T1, T2, Tmin, Tmax, eps_sat;
 
@@ -209,12 +212,12 @@ void CPairs::print_eps(double min, double max, int nmax) {
 	double x, y, dx = (max-min)/nmax, dy = dx;
 
 	// open file
-	ofstream file;
+	std::ofstream file;
 	file.open("energy_density.dat");
 
 	// print grid of energy densities
-	for(y = min; y < max; y += dy){
-		for(x = min; x < max; x += dx){
+	for(y = min; y < max; y += dy) {
+		for(x = min; x < max; x += dx) {
 			file << get_eps(x,y) << "\t";
 		}
 		file << "\n";
